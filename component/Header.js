b348e096870e2998862393993531b50ceee7e929
0xNunana <kudayapaul@gmail.com>
2023-01-24 19:44:08 +0000

    second section

--- component/Header.js
+++ component/Header.js
@@ -35,8 +35,20 @@ function Header() {
             </div>
         
       </div>
-      <div>
-        second part
+      <div className='flex text-white items-center space-x-3 p-2 pl-6 bg-amazon_blue-light'>
+        <p className='link flex items-center'>
+        <Bars3Icon className='h-6 mr-1'/>
+        All
+        </p>
+        <p className='link '>Prime Video</p>
+        <p className='link '>Amazon Business</p>
+        <p className='link '>Today's Deals</p>
+        <p className='link hidden lg:inline-flex '>Electronics</p>
+        <p className='link hidden lg:inline-flex '>Food & Grocery</p>
+        <p className='link hidden lg:inline-flex '>Prime</p>
+        <p className='link hidden lg:inline-flex '>Buy Again</p>
+        <p className='link hidden lg:inline-flex '>Shopper Toolkit</p>
+        <p className='link hidden lg:inline-flex '>Health & Personal Care</p>
       </div>
     </header>
   )
