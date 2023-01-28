4dabe328c9045c4b723c76ce19dac70c55272482
0xNunana <kudayapaul@gmail.com>
2023-01-28 12:50:08 +0000

    basket setup

--- slices/basketSlice.js
+++ slices/basketSlice.js
@@ -8,7 +8,12 @@ export const basketSlice = createSlice({
   name: "basket",
   initialState,
   reducers: {
-    addToBasket: (state, action) => {},
+    addToBasket: (state, action) => {
+      //items where initialized as an array hence we expect them to be stored as an array
+      //store everything sent to the basket(...state.items)
+      //action.payload is the information sent to the basket through dispatch
+      state.items=[...state.items,action.payload]
+    },
     removeFromBasket: (state, action) => {},
   },
 });
