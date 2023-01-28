1ffdf22511cd5e1f3c62b8631041f3c23ec9191d
0xNunana <kudayapaul@gmail.com>
2023-01-28 09:48:29 +0000

    session wrapping

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '@/styles/globals.css'
 import { Provider } from 'react-redux'
 import { store } from '../app/store' 
+//sessionProvide allows for sessions to be active through out all the pages
+import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }) {
-  return ( <Provider store={store}>
+export default function App({ Component, pageProps:{session,...pageProps} }) {
+  return ( <SessionProvider session={session}><Provider store={store}>
       <Component {...pageProps} />
-    </Provider>)
+    </Provider></SessionProvider>
+  )
 }
