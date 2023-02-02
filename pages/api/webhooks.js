e8db909d008be678af3102cc6f545850f70654ec
0xNunana <kudayapaul@gmail.com>
2023-02-02 18:36:47 +0000

    webhook for fullfilment

---
+++ pages/api/webhooks.js
@@ -0,0 +1,68 @@
+import { buffer } from "micro"
+import * as admin from 'firebase-admin'
+
+//Secure connection to FIREBASE from the backend
+const serviceAccount = require("/home/nunana/amazonia/permissions.json")
+const app = !admin.apps.length
+    ? admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    })
+    : admin.app();
+
+//
+const stripe = require('stripe')(process.env.Stripe_secret);
+
+const endpointSecret = process.env.Stripe_signin_secret;
+
+const fulfillOrder = async (session) => {
+    console.log("Fulfilling Order",session);
+    return app.firestore()
+        .collection('users')
+        .doc(session.metadata.email)
+        .collection('orders')
+        .doc(session.id)
+        .set({
+            amount: session.amount_total / 100,
+            amountShipping: session.total_details.amount_shipping / 100,
+            images: JSON.parse(session.metadata.images),
+            timestamp: admin.firestore.FieldValue.serverTimestamp()
+        })
+        .then(() => {
+            console.log(`SUCCESS: Order ${session.id} had been added to the DB`);
+        });
+};
+
+const webhook = async (req, res) => {
+    if (req.method === 'POST') {
+        const requestBuffer = await buffer(req);
+        const payload = requestBuffer.toString();
+        const sig = req.headers["stripe-signature"];
+
+        let event;
+
+        //Verify that the event posted came from STRIPE
+        try {
+            event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+        } catch (err) {
+            console.log('ERROR', err.message);
+            return res.status(400).send(`Webhook error: ${err.message}`)
+        }
+
+        if (event.type === 'checkout.session.completed') {
+            const session = event.data.object;
+
+            //Fulfill the order
+            return fulfillOrder(session)
+            .then(() => {res.status(200)})
+            .catch((err) => {res.status(400).send(`Webhook Error: ${err.message}`)});
+        }
+    }
+};
+export default webhook;
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true,
+    }
+}
